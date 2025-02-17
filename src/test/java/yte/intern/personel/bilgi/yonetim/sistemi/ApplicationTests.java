f7d8764b9dd980d55f10d5afb2a9bc0b97d87688
zsegul <80220476+zsegul@users.noreply.github.com>
2025-02-17 21:45:53 +0300

    Add files via upload

---
+++ src/test/java/yte/intern/personel/bilgi/yonetim/sistemi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package yte.intern.personel.bilgi.yonetim.sistemi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
